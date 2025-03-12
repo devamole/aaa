bd3d47334083056d03c407d1ff437d74ac768f99
devamole <devangelmotta@gmail.com>
2025-03-12 00:45:05 -0500

    +++

--- src/test/java/com/credibanco/cards/CredibanCoCardsServiceApplicationTests.java
+++ src/test/java/com/credibanco/cards/CredibanCoCardsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.credibanco.cards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CredibanCoCardsServiceApplicationTests {
 
 	@Test
